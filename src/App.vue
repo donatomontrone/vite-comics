e85d3f38dfad9b9087eb8a54f25019f000bc0f5e
donatomontrone <donato.montrone@outlook.com>
2023-01-09 15:34:34 +0100

    add inital vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+    <AppMain />
+  </main>
+  <footer>
+    <AppFooter />
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use './styles/general' as *;
+@use './styles/partials/variables' as *; //Anche senza .scss
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <div class="main-logo">
+            <img src="../assets/img/dc-logo.png" alt="DC Comics Main Logo">
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
